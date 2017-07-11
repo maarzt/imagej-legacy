29652c8012e4bc74efd9f87911d033b4b97196df
Alison Walter <awalter2@wisc.edu>
2017-07-11 15:57:45 -0500

    Add default wrapper for ImageJ 1.x Rois to MaskInterval
    And test.

---
+++ src/test/java/net/imagej/legacy/convert/roi/DefaultRoiConversionTest.java
@@ -0,0 +1,200 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2017 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.legacy.convert.roi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Rectangle;
+
+import net.imglib2.Point;
+import net.imglib2.roi.MaskInterval;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ij.gui.EllipseRoi;
+import ij.gui.Line;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
+import ij.gui.RotatedRectRoi;
+
+/**
+ * Tests converting ImageJ 1.x {@link Roi}s to ImgLib2 {@link MaskInterval}, and
+ * the corresponding wrapper {@link DefaultRoiWrapper}.
+ *
+ * @author Alison Walter
+ */
+public class DefaultRoiConversionTest {
+
+	private static Point test;
+
+	@BeforeClass
+	public static void initialize() {
+		test = new Point(new int[] { 0, 0 });
+	}
+
+	@Test
+	public void testDefaultRoiWrapperLineWithWidth() {
+		final Line l = new Line(10, 10, 100, 100);
+		ij.gui.Line.setWidth(5);
+		l.setStrokeWidth(5);
+		final MaskInterval w = new DefaultRoiWrapper<>(l);
+
+		// Check bounds
+		// Changing the line width does not cause the bounding box to update
+		assertEquals(10, w.min(0));
+		assertEquals(10, w.min(1));
+		assertEquals(100, w.max(0));
+		assertEquals(100, w.max(1));
+
+		// Check contains
+		test.setPosition(new int[] { 71, 71 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 12, 16 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 97, 94 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 9, 9 });
+		assertFalse(w.test(test));
+	}
+
+	@Test
+	public void testDefaultRoiWrapperSplineFitPolygonRoi() {
+		final PolygonRoi p = new PolygonRoi(new float[] { 0, 15, 30 }, new float[] {
+			10, 25, 10 }, Roi.POLYGON);
+		p.fitSpline();
+		final MaskInterval w = new DefaultRoiWrapper<>(p);
+
+		// check bounds
+		// spline fitting changes the bounds for a polygon
+		final Rectangle bounds = p.getBounds();
+		assertEquals(bounds.x, w.min(0));
+		assertEquals(bounds.y, w.min(1));
+		assertEquals(bounds.x + bounds.width, w.max(0));
+		assertEquals(bounds.y + bounds.height, w.max(1));
+
+		// Check contains
+		test.setPosition(new int[] { 15, 6 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 5, 20 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 27, 17 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 30, 9 });
+		assertFalse(w.test(test));
+	}
+
+	@Test
+	public void testDefaultRoiWrapperRoundedCornerRectangle() {
+		final Roi r = new Roi(17, -3, 10, 16, 10);
+		final MaskInterval w = new DefaultRoiWrapper<>(r);
+
+		// check bounds
+		assertEquals(17, w.min(0));
+		assertEquals(-3, w.min(1));
+		assertEquals(27, w.max(0));
+		assertEquals(13, w.max(1));
+
+		// Check contains
+		test.setPosition(new int[] { 20, 11 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 22, -1 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 17, 12 });
+		assertFalse(w.test(test));
+
+		test.setPosition(new int[] { 26, 12 });
+		assertFalse(w.test(test));
+
+		test.setPosition(new int[] { 17, -3 });
+		assertFalse(w.test(test));
+
+		test.setPosition(new int[] { 26, -3 });
+		assertFalse(w.test(test));
+	}
+
+	@Test
+	public void testDefaultRoiWrapperEllipseRoi() {
+		final EllipseRoi e = new EllipseRoi(10, 11, 20, 21, 0.5);
+		final MaskInterval w = new DefaultRoiWrapper<>(e);
+
+		// check bounds
+		// bounds are not the minimal bounds
+		assertEquals(9, w.min(0));
+		assertEquals(10, w.min(1));
+		assertEquals(21, w.max(0));
+		assertEquals(22, w.max(1));
+
+		// Check contains
+		test.setPosition(new int[] { 10, 11 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 18, 21 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 19, 14 });
+		assertFalse(w.test(test));
+	}
+
+	@Test
+	public void testDefaultRoiWrapperRotatedRectRoi() {
+		final RotatedRectRoi rrr = new RotatedRectRoi(-3.5, 27, 30.5, 61, 6);
+		final MaskInterval w = new DefaultRoiWrapper<>(rrr);
+
+		// check bounds
+		assertEquals(-6, w.min(0));
+		assertEquals(24, w.min(1));
+		assertEquals(33, w.max(0));
+		assertEquals(63, w.max(1));
+
+		// Check contains
+		test.setPosition(new int[] { 31, 58 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { -1, 32 });
+		assertTrue(w.test(test));
+
+		test.setPosition(new int[] { 1, 26 });
+		assertFalse(w.test(test));
+	}
+
+}
